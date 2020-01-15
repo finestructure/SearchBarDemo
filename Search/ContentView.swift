530adbd8e8d0ddb9c9cfd7824b2f80d624922960
Sven A. Schmidt <sas@finestructure.co>
2020-01-15 11:21:33 +0100

    Working version

--- Search/ContentView.swift
+++ Search/ContentView.swift
@@ -6,17 +6,103 @@
 //  Copyright © 2020 finestructure. All rights reserved.
 //
 
+import Combine
+import Foundation
 import SwiftUI
 
+struct Item: Decodable {
+    let name: String
+    let fullName: String
+}
+
+struct SearchResult: Decodable {
+    let totalCount: Int
+    let incompleteResults: Bool
+    let items: [Item]
+}
+
+enum SearchError: Error {
+    case publisherError
+    case invalidURL
+    case invalidServerResponse
+    case decodingError
+}
+
+let queryURL: (String) -> AnyPublisher<URL, Error> = { query in
+    Just(query)
+        .tryMap { query -> URL in
+            guard let url = URL(string: "https://api.github.com/search/repositories?q=\(query)") else {
+                throw SearchError.invalidURL
+            }
+            return url
+    }
+        .eraseToAnyPublisher()
+}
+
+let dataTask: (URL) -> AnyPublisher<SearchResult, Error> = { url in
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    return URLSession.shared.dataTaskPublisher(for: url)
+        .tryMap { data, response -> Data in
+            guard let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200 else {
+                    throw SearchError.invalidServerResponse
+            }
+            return data
+    }
+    .decode(type: SearchResult.self, decoder: decoder)
+    .mapError { _ in
+        SearchError.decodingError
+    }
+    .eraseToAnyPublisher()
+}
+
+// https://stackoverflow.com/questions/56879734/convert-a-state-into-a-publisher
+final class GithubSearchRequest: ObservableObject {
+    @Published var query: String = "" {
+        didSet { fetch() }
+    }
+    @Published var results = [String]()
+
+    var debounceDelay: Double
+
+    private var currentRequest: AnyCancellable? = nil
+
+    init(debounceDelay: Double = 0.5) {
+        self.debounceDelay = debounceDelay
+    }
+
+    private func fetch() {
+        let req = $query
+            .mapError { _ in SearchError.publisherError }
+            .removeDuplicates()
+            .debounce(for: .seconds(debounceDelay), scheduler: RunLoop.main)
+            .flatMap(queryURL)
+            .flatMap(dataTask)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { result in
+                switch result {
+                    case .finished:
+                        break
+                    case let .failure(error):
+                        print("error: \(error.localizedDescription)")
+                }
+            }, receiveValue: { value in
+                print(value)
+                self.results = value.items.map { $0.fullName }
+            })
+        currentRequest = req
+    }
+}
+
 struct ContentView: View {
-    @State var searchText = ""
-    var results = ["A", "B", "C"]
+    @ObservedObject var ghSearch = GithubSearchRequest(debounceDelay: 0.8)
 
     var body: some View {
         VStack {
             HStack {
                 Image(systemName: "magnifyingglass")
-                TextField("Type to search", text: $searchText)
+                TextField("Type to search", text: $ghSearch.query)
             }
             .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
             .foregroundColor(.secondary)
@@ -24,7 +110,7 @@ struct ContentView: View {
             .cornerRadius(8)
 
             List {
-                ForEach(results.filter { $0.contains(searchText) || searchText == "" }, id: \.self) {
+                ForEach(ghSearch.results, id: \.self) {
                     Text($0)
                 }
             }
